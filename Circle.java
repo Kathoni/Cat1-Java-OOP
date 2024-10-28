75b7343f42d3b03e476fe0f5aa261e027fe0bd4d
Kathoni <thrusherkeys@gmail.com>
2024-10-28 08:44:06 +0300

    modifying circle

--- Circle.java
+++ Circle.java
@@ -1,14 +1,25 @@
-public class Circle{
-    String color ;
-    double radius;
-    public Circle(String color, double radius){
-        this.color = color;
-        this.radius = radius;
-    }
-    public double getArea(){
-        return Math.PI * radius * radius;
-    }
-    public String geString(){
-        return color + " " + radius;
-    }
+public class Circle { 
+ private double radius;
+ private String color;
+
+ 
+ public Circle() { 
+ radius = 1.0;
+ color = "red";
+ }
+
+ 
+ public Circle(double r) { 
+ radius = r;
+ color = "red";
+ }
+
+ 
+ public double getRadius() {
+ return radius;
+ }
+
+ public double getArea() {
+ return radius*radius*Math.PI;
+ }
 }
