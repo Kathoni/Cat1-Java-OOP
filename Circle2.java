56dc02f3d8db9f26be2b8413dfbcdce17ee2045e
Kathoni <thrusherkeys@gmail.com>
2024-10-28 13:30:06 +0300

    Circle 2

---
+++ Circle2.java
@@ -0,0 +1,12 @@
+public class Circle2{
+    double radius;
+    public Circle2(double radius){
+        this.radius=radius;
+    }
+    public double getRadius(){
+       return radius;
+    }
+    public void setRadius(double newRadius){
+    }
+    
+}
