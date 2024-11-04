cdabdb161503cfee17f47fe48f268a30d5653cf2
Kathoni <thrusherkeys@gmail.com>
2024-11-04 13:50:53 +0300

    Time class

---
+++ Time.java
@@ -0,0 +1,78 @@
+public class Time {
+    int hour;
+    int minute;
+    int second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setHour(int newHour) {
+        this.hour = newHour;
+    }
+
+    public void setMinute(int newMinute) {
+        this.minute = newMinute;
+    }
+
+    public void setSecond(int newSecond) {
+        this.second = newSecond;
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Time: %02d:%02d:%02d", hour, minute, second);
+    }
+
+    public Time nextSecond() {
+        second++;
+        if (second == 60) {
+            second = 0;
+            minute++;
+            if (minute == 60) {
+                minute = 0;
+                hour++;
+                if (hour == 24) {
+                    hour = 0;
+                }
+            }
+        }
+        return this;
+    }
+
+    public Time previousSecond() {
+        second--;
+        if (second < 0) {
+            second = 59;
+            minute--;
+            if (minute < 0) {
+                minute = 59;
+                hour--;
+                if (hour < 0) {
+                    hour = 23;
+                }
+            }
+        }
+        return this;
+    }
+}
