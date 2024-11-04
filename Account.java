7ae23868c07a1b46de519bd9d7d64b3493ab92fc
Kathoni <thrusherkeys@gmail.com>
2024-11-04 11:55:40 +0300

    Account error fixed

--- Account.java
+++ Account.java
@@ -3,14 +3,14 @@ public class Account {
     String name;
     int balance;
 
-    public Account( String id, String name, int  balance) {
+    public Account(String id, String name, int balance) {
         this.id = id;
         this.name = name;
         this.balance = balance;
-    }   
+    }
 
     public String getId() {
-      return id;
+        return id;
     }
 
     public String getName() {
@@ -20,26 +20,46 @@ public class Account {
     public int getBalance() {
         return balance;
     }
-    public void setId( String newId){
-        this.id= newId ;
+
+    public void setId(String newId) {
+        this.id = newId;
     }
 
-    public void setName( String newName){
-        this.name= newName ;
+    public void setName(String newName) {
+        this.name = newName;
     }
 
-    public void setBalance( int newBalance){
-        this.balance= newBalance ;
+    public void setBalance(int newBalance) {
+        this.balance = newBalance;
     }
-   
 
+    public void credit(int amount) {
+        balance += amount;
+    }
+
+    public void debit(int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Amount exceeded balance.");
+        }
+    }
+
+    public void transferTo(Account otherAccount, int amount) {
+        if (amount <= balance) {
+            this.debit(amount);
+            otherAccount.credit(amount);
+        } else {
+            System.out.println("Amount exceeded balance.");
+        }
+    }
 
     @Override
     public String toString() {
         return "Account{" +
-                "id is:" + id + "\n" +
-                " name is :'" + name + "\n" +
-                " balance :" + balance ;
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
     }
-    
 }
